e503654ecd6c5261630b96effc67e76f0e79f8f7
Augusto Paulet <augusto.paulet@dinocloudconsulting.com>
2022-05-17 15:01:23 -0300

    #2

--- backend.tf
+++ backend.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    encrypt = "true"
+    region  = "us-east-1"
+    key     = "bda-mlops-feature-store"
+  }
 }
 
 provider "aws" {
